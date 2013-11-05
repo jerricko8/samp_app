f0316d37fe4f3de7fcdc8b26bec32ea80aa8964a
Jerricko <getjerricko@gmail.com>
2013-11-05 14:41:17 -0500

    Finish layouts and routes

--- spec/requests/static_pages_spec-MOD.rb
+++ spec/requests/static_pages_spec-MOD.rb
@@ -1,31 +1,35 @@
 require 'spec_helper'
+
 describe "Static pages" do
-
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
-	def page_attr(page_describe, page_title, page_content)
-
-		describe "#{page_describe} page" do
-			it "should have the content '#{page_content}'" do
-				visit "/static_pages/#{page_title}".downcase
-				expect(page).to have_content("#{page_content}")
-			end
-
-			it "should have the title '#{page_title}" do
-				visit "static_pages/#{page_title}".downcase
-				expect(page).to have_title("#{base_title} | #{page_title}")
-			end
-		end		
 	
+	subject { page }
 
-		# if below methods are outside def block, rspec gives errors
-		# else it runs OK which doesn't make sense I think. 
-		#Find out WHY? do we need to define a class first?
-		page_attr("Home", "home", "Sample App")
-		page_attr("About Us", "about", "About Us")
-		page_attr("Help", "help", "Help")
-		page_attr("Contact Us", "contact", "Contact Us")
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_content('Sample App') }	
+		it { should have_title(full_title('')) }	
+		it { should_not have_title('| Home') }		
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }		
+	end
+
+	describe "About page" do
+		before { visit about_path }
+
+		it { should have_content('About') }
+		it { should have_title(full_title('About Us')) }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_selector('h1', text: 'Contact') }
+		it { should have_title(full_title('Contact')) }		
 	end
 end
-
-
